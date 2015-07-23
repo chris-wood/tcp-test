97dd138a9a00f32faae4c877798215f6114330de
Christopher Wood <christopherwood07@gmail.com>
2015-07-22 21:53:05 -0700

    udp server

---
+++ src/udp/udp-server.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "../util.h"
+
+int
+main(int argc, char **argv)
+{
+    // TODO
+
+    return 0;
+}
