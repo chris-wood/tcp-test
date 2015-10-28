cdd16356f9b7d51cd068cdb6aa18f335ec9eb9a0
Christopher Wood <christopherwood07@gmail.com>
2015-10-28 07:11:16 -0700

    client experiment script updated

--- src/scripts/start-client.sh
+++ src/scripts/start-client.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 IPADDR=127.0.0.1
-PORT=9001
+PORT=8001
 
 FILENAME=file.bin
+
+SIZES=( 10 100 1000 10000 100000 )
 
 TCPCLIENT=../tcp/tcp-client
 UDPCLIENT=../udp/udp-client
@@ -19,9 +21,18 @@ export TIMEFORMAT='%3R'
 
 for CLIENT in "${CLIENTLIST[@]}"
 do
-    for i in `seq 1 $N`
+    for s in "${SIZES[@]}"
     do
-        OUTFILE=$CLIENT_$i.out
-        $CLIENT $IPADDR $PORT $FILENAME > $OUTFILE
+        for i in `seq 1 $N`
+        do
+            OUTFILE=run_"$s"_$i.out
+
+            echo $SERVER $s $i $OUTFILE
+            echo $CLIENT $IPADDR $PORT $FILENAME
+
+            $CLIENT $IPADDR $PORT $FILENAME > $OUTFILE
+
+            sleep 0.5
+        done
     done
 done
--- src/scripts/start-server.sh
+++ src/scripts/start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IPADDR=127.0.0.1
-PORT=9001
+PORT=8001
 FILENAME=file.bin
 SIZES=( 10 100 1000 10000 100000 )
 
@@ -21,13 +21,16 @@ echo "Starting the server test..."
 
 for SERVER in "${SERVERLIST[@]}"
 do
-    echo "$SERVER"
     for s in "${SIZES[@]}"
     do
         for i in `seq 1 $N`
         do
-            OUTFILE=$SERVER_$i.out
-            ./create-file.sh $FILENAME $s
+            OUTFILE=$SERVER_$s_$i.out
+            ./create-file.sh $FILENAME $s 2> out
+            
+            echo $SERVER $s $i
+            echo $SERVER $PORT $FILENAME
+
             $SERVER $PORT $FILENAME > $OUTFILE
         done
     done
